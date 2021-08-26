2c87718ab8c1b0d0dce6d467e4753eb50f3e25c0
Samila Rodrigues <86678393+samgrs@users.noreply.github.com>
2021-08-25 23:13:48 -0300

    Add files via upload

---
+++ sam/sam.js
@@ -0,0 +1,5 @@
+alert("Seja bem-vindo ao meu site! <3");
+alert("87 + 96 = " + (87 + 96));
+alert("5 x 8 = " + (5 * 8));
+var listaCompras = ["Maçãs", "Bananas", "Uvas", "Tomates"];
+alert(listaCompras);
